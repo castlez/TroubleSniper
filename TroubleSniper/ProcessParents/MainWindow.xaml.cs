39dd070b5a1389679ba82c044462b5297fceb3d3
jonny Castle <castlez@live.com>
2016-07-11 13:55:12 -0700

    implemented processparents, but cant launch due to "process not running" bug
    gonna try some stuff, commiting for safety

--- TroubleSniper/ProcessParents/MainWindow.xaml.cs
+++ TroubleSniper/ProcessParents/MainWindow.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Management;
 
 namespace ProcessParents
 {
@@ -26,6 +28,29 @@ namespace ProcessParents
         public MainWindow()
         {
             InitializeComponent();
+            var processes = Process.GetProcesses();
+
+            Each(processes, i => output_processes.Text += GetParentName(i) + "\r\n\t" + i.ProcessName.ToString() + "\r\n" );
+
+        }
+
+        public void Each<T>(IEnumerable<T> items, Action<T> action)
+        {
+            foreach (var item in items)
+                action(item);
+        }
+
+        public static string GetParentName(Process child)
+        {
+            var myId = child.Id;
+            var query = string.Format("SELECT ParentProcessId FROM Win32_Process WHERE ProcessId = {0}", myId);
+            var search = new ManagementObjectSearcher("root\\CIMV2", query);
+            var results = search.Get().GetEnumerator();
+            results.MoveNext();
+            var queryObj = results.Current;
+            var parentId = (uint)queryObj["ParentProcessId"];
+            var parent = Process.GetProcessById((int)parentId);
+            return parent.ProcessName;
         }
     }
 }
